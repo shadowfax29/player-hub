564c1337d2dc614671d7c4e86b19019c930f62f9
shadowfax29 <shadowfax29@github.com>
2026-08-27 16:42:47 +0530

    fix: increase navbar logo height

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -47,9 +47,9 @@ export function HomeLayout({ children }: { children: React.ReactNode }) {
               <Image
                 src="/Play-removebg-preview.png"
                 alt="PlayConsole"
-                width={140}
-                height={40}
-                className="h-8 w-auto"
+                width={200}
+                height={60}
+                className="h-12 w-auto"
                 priority
               />
             </Link>
